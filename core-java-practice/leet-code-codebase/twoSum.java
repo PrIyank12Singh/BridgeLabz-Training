c981345769d791ef2ed864567232cd5b65d62288
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-17 15:59:00 +0530

    add leet-code-codebase

---
+++ core-java-practice/leet-code-codebase/twoSum.java
@@ -0,0 +1,41 @@
+
+
+import java.util.*;
+
+public class twoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            int n1=nums[i];
+            for(int j=0;j<nums.length;j++){
+               int n2=nums[j];
+                if(i!=j && n1+n2==target){
+                   
+                    return new int[]{i,j};
+                }
+
+            }
+
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int[] ans = twoSum(nums, target);
+        System.out.println(Arrays.toString(ans));
+
+        sc.close();
+    }
+}
+
