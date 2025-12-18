c11581fbd775d0ec1cc30b0ba6aeaa3c2fb56bf2
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 11:25:48 +0530

    Average Marks.java

---
+++ core-java-practice/gcr-codebase/programming-element/average_marks.java
@@ -0,0 +1,13 @@
+
+public class average_marks {
+  public static void main(String[] args) {
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+    
+    double average = (maths+physics+chemistry)/3;
+
+    System.out.println("Average percent of Sam's marks is: "+average+"%");
+  } 
+  
+}
