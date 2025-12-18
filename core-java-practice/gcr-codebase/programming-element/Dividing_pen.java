c0e61a8c5189786c743e706db5dfffd86dbbba80
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 11:28:28 +0530

    Diviiding pens.java

---
+++ core-java-practice/gcr-codebase/programming-element/Dividing_pen.java
@@ -0,0 +1,13 @@
+public class Dividing_pen {
+  public static void main(String[] args) {
+    int pen = 14;
+    int students = 3;
+
+    int p = pen/students;
+    int r = pen%students;
+
+    System.out.println("The Pen per student is "+p+" and the remaining pen not distributed is "+r);
+  }
+  
+  
+}
