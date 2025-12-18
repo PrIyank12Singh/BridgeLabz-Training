5d8bfcedc71023f8e5dde8658a95d6a1d80ac486
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 12:12:27 +0530

    University Fees.java

---
+++ core-java-practice/gcr-codebase/programming-element/UniversityFees.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UniversityFees {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int fee = sc.nextInt();
+
+    int  discount_p = sc.nextInt();
+    int discount = (fee * discount_p)/100;
+
+    int final_fees = fee- discount;
+
+    System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+final_fees);
+
+    sc.close();
+  }
+}
