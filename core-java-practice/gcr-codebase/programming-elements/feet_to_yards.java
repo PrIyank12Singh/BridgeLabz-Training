a15a3629397df3b8e267f9f102569bd79fc60d91
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 15:47:36 +0530

    Feet to Yards.java

--- core-java-practice/gcr-codebase/programming-elements/feet_to_yards.java
+++ core-java-practice/gcr-codebase/programming-elements/feet_to_yards.java
@@ -5,10 +5,10 @@ public class feet_to_yards {
 
     int distance_feet = sc.nextInt();               //taking input in feet
 
-    int distance_yard = distance_feet * 3;          //formula to calculate in yards from feet
+    int distance_yard = distance_feet /3;          //formula to calculate in yards from feet
 
 
-    int distance_mile = distance_yard*1760;          //formula to calculate in miles from yards
+    int distance_mile = distance_yard/1760;          //formula to calculate in miles from yards
 
     System.out.println("The distance is feet is "+distance_feet+" while in yards is "+distance_yard+" and miles is "+distance_mile);
     sc.close();
