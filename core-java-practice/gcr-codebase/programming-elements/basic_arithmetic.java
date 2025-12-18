8369da80094ce10cbaa852093150dec3b0cc0fc8
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 14:35:18 +0530

    Arithmetic operations.java

---
+++ core-java-practice/gcr-codebase/programming-elements/basic_arithmetic.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class basic_arithmetic {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    float number1 = sc.nextInt();   //taking number1 as input 
+    float number2 = sc.nextInt();   //taking number2 as input 
+
+    float addition = number1+number2;   //adding both numbers
+    
+    float subtract = number1-number2;  //subtracting both numbers
+
+    float multiply = number1*number2;  //multiplying both numbers
+
+    float division = number1/number2;   //dividing both numbers
+
+    System.out.println("The addition, subtraction, multiplication and division value of 2 numbers "+number1+" and "+number2+" is "+addition+","+subtract+","+multiply+" and "+division);
+
+    
+
+    
+
+    System.out.println();
+
+    sc.close();
+  }
+}
