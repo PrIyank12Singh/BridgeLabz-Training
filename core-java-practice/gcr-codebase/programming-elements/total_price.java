e0a5424516ea75c797102f48f43a747f0f0f5d9e
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 15:30:20 +0530

    Total price.java

---
+++ core-java-practice/gcr-codebase/programming-elements/total_price.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class total_price {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    double unitPrice = sc.nextDouble();            //taking unit's Price
+
+    int quantity = sc.nextInt();                    //taking quantity that we want of particular unit
+
+    double total_price = unitPrice * quantity;       //calculating total price
+
+    System.out.println("The total purchase price is INR " + total_price+ " if the quantity " +quantity+ " and unit price is INR "+unitPrice);
+    sc.close();
+  }  
+}
