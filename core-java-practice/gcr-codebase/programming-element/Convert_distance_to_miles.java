bb7adca47e4603db2526b7af3fb5808443eb2006
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-18 12:04:44 +0530

    Distance to miles.java

---
+++ core-java-practice/gcr-codebase/programming-element/Convert_distance_to_miles.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Convert_distance_to_miles {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+
+    double km = sc.nextDouble();
+
+    double miles = km * 0.621371;
+
+    System.out.println("Distance in kilometers is "+km+" in miles is"+ miles);
+    sc.close();
+  }
+  
+}
