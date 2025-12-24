2c400fe910e2d7466f0643dab4520a980d4804cc
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-24 12:22:57 +0530

    [Priyank Singh] solved all levels of arrays

--- core-java-practice/gcr-codebase/arrays/multiplication_of_number.java
+++ core-java-practice/gcr-codebase/arrays/multiplication_of_number.java
@@ -4,6 +4,7 @@ public class multiplication_of_number {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    //taking number as a input
     int n = sc.nextInt();
 
     int num = n;
@@ -13,7 +14,7 @@ public class multiplication_of_number {
     //printing the table of input number 
 
     for (int i = 1; i < a.length; i++) {
-      
+
       a[i] = num*i;
       System.out.println(num+" * "+i+ " = "+ a[i]);
     }
