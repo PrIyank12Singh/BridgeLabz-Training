9f60eda0c7007d89ea4612ef7df16d33aa920066
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2025-12-30 22:17:37 +0530

    [Priyank Singh] solved Snakes and Ladder problem

---
+++ core-java-practice/scenario-based/SnakesAndLadder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SnakesAndLadder {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    int player1 = 0;
+    int player2 = 0;
+
+    
+    sc.close();
+  }
+}
