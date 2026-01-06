22b36d8a452af0929e2606b0d767700e772b0e37
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-01-06 11:17:31 +0530

    [Priyank Singh] solved Palindrome Number

--- core-java-practice/leet-code-codebase/remove_element.java
+++ core-java-practice/leet-code-codebase/remove_element.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
-class remove_element {
-    public int removeElement(int[] nums, int val) {
+class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
         int index = 0;
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
                 nums[index] = nums[i];
@@ -13,16 +15,22 @@ class remove_element {
     }
 
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
-      int[] nums= new int[n];
-      int val = sc.nextInt();
 
-      for (int i = 0; i < nums.length; i++) {
-        nums[i] = sc.nextInt();
-      }
+        Scanner sc = new Scanner(System.in);
 
-      int answer = removeElement( nums[],val)
-      sc.close();
+        int n = sc.nextInt();          // size of array
+        int[] nums = new int[n];
+
+        int val = sc.nextInt();        // value to remove
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int answer = removeElement(nums, val);
+
+        System.out.println(answer);
+
+        sc.close();
     }
 }
