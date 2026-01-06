9cb3e6b42efc5e35e4da228615c6c9257d678e80
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-01-06 15:44:50 +0530

    [Priyank Singh] solved Sort 0,1,2 problem

---
+++ Sort_0_1_2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Sort_0_1_2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length; j++) {
+        if(arr[j-1]>arr[j]){
+          int temp = arr[j-1];
+          arr[j-1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+
+    for (int i:arr) {
+      System.out.print(" "+i);
+    }
+    sc.close();
+  }
+}
