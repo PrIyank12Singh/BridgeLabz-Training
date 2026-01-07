5fd40aaf1de1b5d9859cc80374ec54c6873b1ccc
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-01-07 16:18:07 +0530

    [Priyank] performed Delete Operation

--- scenario-based/PeakFareCalculator.java
+++
@@ -1,6 +0,0 @@
-public class PeakFareCalculator implements FareCalculator {
-    @Override
-    public double calculateFare(double distance) {
-        return distance * 15; // 15 per km (peak)
-    }
-}
