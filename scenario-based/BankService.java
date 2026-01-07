958e91a8f9e64aa2ea2df571c7a47bfb56856218
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-01-07 16:10:33 +0530

    [Priyank] performed Delete Operation

--- scenario-based/BankService.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface BankService {
-    void createAccount(Account account);
-    double getBalance(String accountId);
-    void transfer(String fromAccountId, String toAccountId, double amount) throws InsufficientBalanceException;
-    List<Transaction> getTransactionHistory(String accountId);
-}
