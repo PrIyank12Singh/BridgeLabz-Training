dc635b5e5b3b0a90d8a3c0870173d8a60d0519db
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-01-31 17:01:19 +0530

    [Priyank] added J-Unit Testing problems

---
+++ gcr-codebase/junit/src/main/StringUtils.java
@@ -0,0 +1,28 @@
+public class StringUtils {
+
+
+    public String reverse(String str) {
+        if (str == null) {
+            return null;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    
+    public boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        String cleaned = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+
+  
+    public String toUpperCase(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.toUpperCase();
+    }
+}
